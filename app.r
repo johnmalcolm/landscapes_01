d9c3044c2b73b3df0128d58058b05e925464f96f
John Malcolm Anderson <hi@johnmalcolmdesign.com>
2019-03-30 20:34:01 +0000

    added isolate, reactive & observe functions

--- app.r
+++ app.r
@@ -1,16 +1,39 @@
 library(shiny)
 
 ui <- fluidPage(
- sliderInput(inputId = "num",
-	 label = "Choose a number",
-	 value = 25, min = 1, max = 100),
-	 plotOutput("hist")
+	sliderInput(
+		inputId = "num",
+		label = "Choose a number",
+		value = 25, min = 1, max = 100
+	 ),
+	textInput(
+		inputId = "title",
+		label = "write a title",
+		value = "histogram of random normal values"
+	),
+	actionButton(
+		inputId = "clicks",
+		label = "click me"
+	),
+	plotOutput("hist"),
+	verbatimTextOutput("stats")
 )
 
 server <- function(input, output) {
- output$hist <- renderPlot({
- 	hist(rnorm(input$num))
- })
+	data <- reactive({
+		rnorm(input$num)
+	})
+
+	output$hist <- renderPlot({
+	 	hist(data(), main = isolate(input$title))
+	})
+
+	output$stats <- renderPrint({
+	 	summary(data())
+	})
+
+	# Same as observe({print(as.numeric(input$clicks)})
+	observeEvent(input$clicks, print(as.numeric(input$clicks)))
 }
 
 shinyApp(ui = ui, server = server)
